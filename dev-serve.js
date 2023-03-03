cc1d52df0a62c620bda5ece3f752e57cddf626c4
andrew_wang <andrew7810262000@gmail.com>
2023-03-03 17:57:20 +0800

    delete redundant code in dev-serve.js

--- dev-serve.js
+++ dev-serve.js
@@ -1,5 +1,4 @@
 // https://vitejs.dev/guide/ssr.html
-import fs from "fs";
 import fse from "fs-extra";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -25,7 +24,7 @@ class BrowserSyncHelper {
 
     viteDevServer.watcher.on("change", (file) => {
       console.log(`File ${file} has been changed`);
-      if ( file.includes('/src/') || file.includes('/public/') ) bs.reload();
+      if (file.includes("/src/") || file.includes("/public/")) bs.reload();
     });
   }
 }
@@ -64,28 +63,18 @@ async function createServer() {
     const url = req.originalUrl;
 
     try {
-      // 1. 读取 index.html
-      let template = fs.readFileSync(
-        path.resolve(__dirname, "index.html"),
-        "utf-8"
-      );
 
-      // 2. 应用 Vite HTML 转换。这将会注入 Vite HMR 客户端，
-      //    同时也会从 Vite 插件应用 HTML 转换。
-      //    例如：@vitejs/plugin-react 中的 global preambles
-      template = await vite.transformIndexHtml(url, template);
-
-      // 3. 加载服务器入口。vite.ssrLoadModule 将自动转换
+      // 1. 加载服务器入口。vite.ssrLoadModule 将自动转换
       //    你的 ESM 源码使之可以在 Node.js 中运行！无需打包
       //    并提供类似 HMR 的根据情况随时失效。
       const { render } = await vite.ssrLoadModule(entryServer);
 
-      // 4. 渲染应用的 HTML。这假设 entry-server.js 导出的 `render`
+      // 2. 渲染应用的 HTML。这假设 entry-server.js 导出的 `render`
       //    函数调用了适当的 SSR 框架 API。
       //    例如 ReactDOMServer.renderToString()
       const [appHtml] = await render(url);
 
-      // 4.1 無對應頁面時，回覆沒有此頁面
+      // 3. 無對應頁面時，回覆沒有此頁面
       if (appHtml === "<!---->") {
         return res
           .status(200)
@@ -93,12 +82,12 @@ async function createServer() {
           .end("no this page");
       }
 
-      // 5. 注入渲染后的应用程序 HTML 到模板中。
+      // 4. 注入渲染后的应用程序 HTML 到模板中。
+      const template = `<mjml><!--app-html--></mjml>`;
       const mjml = template.replace(`<!--app-html-->`, appHtml);
-
       const html = mjml2html(mjml).html;
 
-      // 6. 返回渲染后的 HTML。
+      // 5. 返回渲染后的 HTML。
       res.status(200).set({ "Content-Type": "text/html" }).end(html);
     } catch (e) {
       // 如果捕获到了一个错误，让 Vite 来修复该堆栈，这样它就可以映射回
