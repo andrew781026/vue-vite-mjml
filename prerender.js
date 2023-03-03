e077e208983ba21ad443dae87478231b06359a36
andrew_wang <andrew7810262000@gmail.com>
2023-03-03 16:56:34 +0800

    fix prerender.js error

--- prerender.js
+++ prerender.js
@@ -2,7 +2,7 @@
 // run `npm run generate` and then `dist/static` can be served as a static site.
 // ref : https://github.com/vitejs/vite-plugin-vue/blob/main/playground/ssr-vue/prerender.js
 
-import fs from "node:fs";
+import fse from "fs-extra";
 import path from "node:path";
 import url from "node:url";
 import mjml2html from "mjml";
@@ -13,14 +13,10 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 const toAbsolute = (p) => path.resolve(__dirname, p);
 
-const template = fs.readFileSync(
-  toAbsolute(`${distFolder}/index.html`),
-  "utf-8"
-);
-const { render } = await import("./dist/server/entry-server.js");
+const template = fse.readFileSync(toAbsolute(`index.html`), "utf-8");
 
 // determine routes to pre-render from src/pages
-const routesToPrerender = fs
+const routesToPrerender = fse
   .readdirSync(toAbsolute("src/pages"))
   .filter((file) => /.vue$/.test(file))
   .map((file) => {
@@ -29,6 +25,8 @@ const routesToPrerender = fs
   });
 
 export const prerender = async () => {
+  const { render } = await import("./dist/server/entry-server.js");
+
   // pre-render each route...
   for (const url of routesToPrerender) {
     console.log("url:", url);
@@ -37,19 +35,13 @@ export const prerender = async () => {
     const mjml = template.replace(`<!--app-html-->`, appHtml);
 
     const mjmlPath = `${path.resolve(distFolder, "../mjml")}${url}.mjml`;
-    fs.writeFileSync(toAbsolute(mjmlPath), mjml);
+    await fse.writeFile(toAbsolute(mjmlPath), mjml);
 
     const html = mjml2html(mjml).html;
     const htmlPath = `${path.resolve(distFolder, "../html")}${url}.html`;
-    fs.writeFileSync(toAbsolute(htmlPath), html);
+    await fse.writeFile(toAbsolute(htmlPath), html);
 
-    fs.readdirSync(publicFolder).forEach((file) => {
-      fs.copyFileSync(
-        path.resolve(publicFolder, file),
-        path.resolve(distFolder, "../html/assets", file)
-      );
-    });
-
+    await fse.copy(publicFolder, path.resolve(distFolder, "../html/assets"));
     console.log("pre-rendered:", mjmlPath);
   }
 };
